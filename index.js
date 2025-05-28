6f94998d8bc2af789246ea8ad9d396d35a27796a
shegiselle <giselle.pfigueiredo@gmail.com>
2025-05-28 23:44:34 +0000

    fix: define app com express()

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require("express");
+const app = express(); // <- isso define o app
 const port = process.env.SERVER_PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Evolution API funcionando com Express!");
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
