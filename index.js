47b9340b849e18089f6f48d43c407c09ebb51508
shegiselle <giselle.pfigueiredo@gmail.com>
2025-05-29 00:33:18 +0000

    Deploy API Express

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ app.get("/", (req, res) => {
   res.send("Evolution API funcionando com Express!");
 });
 
+// Servir o painel manager
+app.use("/manager", express.static("public/manager"));
+
 const port = process.env.SERVER_PORT || 8080;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
