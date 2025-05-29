45b614faccbb96d45f15b1c733428e8e76857c44
shegiselle <giselle.pfigueiredo@gmail.com>
2025-05-29 00:06:39 +0000

    Deploy API Express

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const app = express(); // <- isso define o app
-const port = process.env.SERVER_PORT || 8080;
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Evolution API funcionando com Express!");
 });
 
+const port = process.env.SERVER_PORT || 8080;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
